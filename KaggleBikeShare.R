22bf4821689e4f79e1bbbaf9419371a00acfab6a
Chase Nielsen <chasemnielsen@gmail.com>
2025-09-16 16:27:12 -0600

    Homework 4

--- KaggleBikeShare.R
+++ KaggleBikeShare.R
@@ -4,9 +4,13 @@ library(vroom)
 library(GGally)
 library(patchwork)
 library(tidymodels)
+library(recipes)
+library(lubridate)
 
 # ---- Read in data ----
 train <- vroom("data/train.csv")
+test <- vroom("data/test.csv")
+
 head(train)
 summary(train)
 
@@ -63,5 +67,47 @@ plot <- (p1 | p2) / (p3 | p4)
 
 ggsave("data/bike_share_eda.pdf", plot = plot, width = 10, height = 8)
 
+# Linear regression
 
+train <- train %>% mutate(log_count = log1p(count))
+
+x <- intersect(
+  setdiff(names(train), c("log_count", "count", "datetime", "casual", "registered")),
+  names(test)
+)
+
+lm_model <- lm(reformulate(x, response = "log_count"), data = train)
+
+log_preds   <- predict(lm_model, newdata = test[, x, drop = FALSE])
+count_preds <- pmax(0, round(expm1(log_preds)))
+
+submission <- tibble(
+  datetime = format(as.POSIXct(test$datetime), "%Y-%m-%d %H:%M:%S"),
+  count    = as.integer(count_preds)
+)
+
+vroom::vroom_write(submission, "submission.csv", delim = ",")
+
+
+# Homework
+train <- vroom("data/train.csv") %>%
+  mutate(
+    datetime  = ymd_hms(datetime, tz = "UTC"),
+    log_count = log1p(count)
+  ) %>%
+  select(-casual, -registered, -count)
+
+# Recipe
+mybike_recipe <- recipe(log_count ~ ., data = train) %>%
+  step_mutate(weather = ifelse(weather == 4, 3, weather)) %>%
+  step_mutate(weather = factor(weather)) %>%
+  step_time(datetime, features = c("hour")) %>%
+  step_corr(all_numeric_predictors(), threshold = 0.7) %>%
+  step_zv(all_predictors())
+
+# Prep & bake
+mybike_prep  <- prep(mybike_recipe, training = train)
+baked_data   <- bake(mybike_prep, new_data = NULL)
+
+head(baked_data, 5)
 
